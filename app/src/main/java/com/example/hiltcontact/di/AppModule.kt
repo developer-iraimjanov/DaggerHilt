67682383b239260c9318868587616b43d36a8a24
Iqboljon1 <iqboljon44444@gmail.com>
2022-10-18 18:26:10 +0300

    DaggerHilt

--- app/src/main/java/com/example/hiltcontact/di/AppModule.kt
+++ app/src/main/java/com/example/hiltcontact/di/AppModule.kt
@@ -8,6 +8,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -19,8 +21,12 @@ object AppModule {
     @Provides
     @Singleton
     fun provideMyApi(): MyApi {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
         return Retrofit.Builder()
             .baseUrl("https://usersiqboljon.herokuapp.com/")
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MyApi::class.java)
